b08c4516a39deefa90bcc70c7bf996c40eb972b2
Andrew Nagle <andrew@Andrews-MacBook-Pro.local>
2017-10-11 11:20:51 -0500

    Commit 3: Magic 8 Ball express application created

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
+app.use(express.static('public'));
+
 app.get('/greeting/', (req, res) => {
 	res.send('Hello, stranger');
 });
@@ -27,6 +29,13 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 });
 
 
+//Magic 8 Ball
+const magicArr = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+
+app.get('/magic/:q', (req, res) => {
+	const answer = Math.floor(Math.random()*magicArr.length)
+	res.send(req.params.q + '?'  + magicArr[answer]);
+});
 
 
 
